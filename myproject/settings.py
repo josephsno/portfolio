37c656ee141b9bbc78e72d1d5607379ffafa8ad3
Ariri Joseph <josephariri@afripointgroup.com>
2024-05-06 16:49:44 +0100

    first commit - committing all files to the repository

--- myproject/settings.py
+++ myproject/settings.py
@@ -86,7 +86,7 @@ STATIC_URL = "/static/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
