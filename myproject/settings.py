f27b36596176a3721bedb16c7cb146885329e29d
Ariri Joseph <josephariri@afripointgroup.com>
2024-06-04 14:22:54 +0100

    first commit - committing all files to the repository

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "USER": "joe",
-#         "NAME": "josep",
-#         "PASSWORD": "1234qw",
-#         "HOST": "localhost",
-#         "PORT": 5432,
-#     },
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": "joe",
+        "NAME": "josep",
+        "PASSWORD": "1234qw",
+        "HOST": "localhost",
+        "PORT": 5432,
+    },
+}
 
 
 
